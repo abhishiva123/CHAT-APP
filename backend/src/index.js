2cf646271f90574815e7a2479d1c29080d3b97b5
Shiva <abhishiva684@gmail.com>
2025-09-12 17:29:04 +0530

    final2

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,14 +20,14 @@ app.use(cors({
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
-//   if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// app.get("/:catchAll(.*)", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-//   });
+app.get("/:catchAll(.*)", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 
-// }
+}
 
 
 server.listen(PORT,()=>{
